70e9ab0b87abacc6cd530b077bb23ec5d6f997dc
afropuku <69128925+afropuku@users.noreply.github.com>
2020-10-20 10:17:58 +0900

    シークレットの追加

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+from .settings_secret import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@%p&mg2vy^i6pqiody1!t+l+!tjb$q9!hzg_9ch)_-35i#!=4e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
